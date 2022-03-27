db6a45f2710932d3b1f1f5876318c56f8d089db1
Paola Anaya <p.andrea.a93@gmail.com>
2022-03-27 10:06:14 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Wheel from "./wheel";
-import img from "./assets/img/logo.png";
 import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
+import { Col, Grid, Row } from "react-flexbox-grid";
 
 const App = () => {
   const [winner, setWinner] = useState();
+  let subtitle;
   const { width, height } = useWindowSize();
-
-  return (
-    <div className="app-info">
-      {winner ? <Confetti width={width} height={height} /> : null}
-      <img className="img-logo" src={img} />
-      <Wheel setWinner={setWinner} />
-      <div className="winner-title">{winner ? "Felicidades" : null}</div>
+  const backgroundColor = "rgba(2, 36, 153, 0.7)";
+  const gradient =
+    "linear-gradient(0deg, rgba(30, 22, 129, 1) 25%, rgba(125, 117, 219, 1) 65%)";
+  const title = () => (
+    <div >
+      <div className="winner-title"> {"Felicidades"} </div>
       <div className="winner">{winner}</div>
+      <button
+        style={{
+          width: "100%",
+          height: "4hv",
+          background: "transparent",
+          color: "white",
+          fontSize: "6hv",
+          border: "transparent",
+        }}
+        onClick={() => setWinner(null)}
+      >
+        {"Volver a tirar"}
+      </button>
+    </div>
+  );
+  return (
+    <div className="app-container">
+      <div className={winner ? "winner-container" : null}>{winner ? title() : null}</div>
+      <Grid style={{ width: "100%", paddingTop: "15%" }}>
+        <Row>
+          <Col xs={12} />
+        </Row>
+        <Row style={{ height: height / 2 }}>
+          <Col
+            xs={3}
+            style={{
+              background: backgroundColor,
+              paddingRight: 0,
+            }}
+          />
+          <Col xs={6} style={{ padding: 0 }}>
+            <div>
+              <div className="app-info">
+                {winner ? <Confetti width={width} height={height} /> : null}
+                <Wheel setWinner={setWinner} />
+              </div>
+              <div className="side" />
+            </div>
+          </Col>
+          <Col
+            xs={3}
+            style={{
+              background: backgroundColor,
+            }}
+          />
+        </Row>
+        <Row>
+          <Col xs={12} style={{ height: "50%" }} />
+        </Row>
+      </Grid>
     </div>
   );
 };
--- src/wheel.js
+++ src/wheel.js
@@ -5,6 +5,8 @@ import RoulettePro from "react-roulette-pro";
 import "react-roulette-pro/dist/index.css";
 
 import prizes from "./csvjson.json";
+import titulo from "./assets/img/gigos.png";
+import "./wheel.css"
 
 const reproductionArray = (array = [], length = 0) => [
   ...Array(length)
@@ -12,10 +14,26 @@ const reproductionArray = (array = [], length = 0) => [
     .map(() => array[Math.floor(Math.random() * array.length)]),
 ];
 
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  }
+  // Directly return the joined string
+  return splitStr.join(' '); 
+}
+
+const newPrizes = prizes.map((element) => ({
+  text:  titleCase(element.text),
+  image: titulo,
+}));
+
 const prizeList = [
-  ...prizes,
+  ...newPrizes,
   ...reproductionArray(prizes, prizes.length * 2),
-  ...prizes,
+  ...newPrizes,
   ...reproductionArray(prizes, prizes.length),
 ];
 
@@ -26,7 +44,6 @@ function generateRandomInteger(max) {
 const Wheel = (props) => {
   const { setWinner } = props;
   const [start, setStart] = useState(false);
-  const [name, setName] = useState();
   const [prizeIndex, setPrizeIndex] = useState();
   const [title, setTitle] = useState("Girar");
 
@@ -43,23 +60,32 @@ const Wheel = (props) => {
   };
 
   return (
-    <div>
+    <div style={{ fontSize: 16 }}>
       <RoulettePro
         prizes={prizeList}
         prizeIndex={prizeIndex}
+        designOptions={{
+          prizeItemWidth: 90,
+          prizeItemHeight: 90,
+        }}
+        style={{
+          fontSize: 14,
+        }}
         start={start}
         onPrizeDefined={handlePrizeDefined}
+        spinningTime={3}
         design={"GracefulLines"}
       />
-      <div style={{ width: "100", alignSelf: "center", margin: "auto" }}>
+      <div style={{ width: "120", alignSelf: "center", margin: "auto" }}>
         <button
           style={{
+            marginTop: -10,
             width: "100%",
             height: "42px",
             background: "transparent",
             color: "white",
-            fontSize: '36px',
-            border: 'transparent'
+            fontSize: "36px",
+            border: "transparent",
           }}
           onClick={handleStart}
         >
